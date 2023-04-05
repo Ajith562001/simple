e2588c5df60f62dae8ba5b8187af8abecbba2f62
Ajith562001 <ajith562001@gmail.com>
2023-04-05 12:21:02 +0530

    git action demo

---
+++ GitAction/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str, rev;
+        Console.WriteLine("Enter a string: ");
+        str = Console.ReadLine();
+        char[] ch = str.ToCharArray();
+        Array.Reverse(ch);
+        rev = new string(ch);
+        if (str == rev)
+            Console.WriteLine(str + " is a palindrome.");
+        else
+            Console.WriteLine(str + " is not a palindrome.");
+        Console.ReadLine();
+    }
+}
